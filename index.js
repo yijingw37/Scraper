1825cbf14a67f0627521e1c8fd72c8035d00a90f
yijingw37 <31359897+yijingw37@users.noreply.github.com>
2022-10-27 19:44:28 -0500

    generate courses.json

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
 const curl = require("curl");
 const jsdom = require("jsdom");
+const fs = require('fs');
+const { JSDOM } = jsdom;
+const dom = new JSDOM(html);
+const $ = (require('jquery'))(dom.window);
 const url = "https://www.mccormick.northwestern.edu/computer-science/academics/courses/";
-curl.get(url, null, (err,resp,body)=>{
-  if(resp.statusCode == 200){
-     parseData(body);
-  }
-  else{
-     //some error handling
-     console.log("error while fetching url");
-  }
+
+curl.get(url, null, (err, resp, body) => {
+    if (resp.statusCode == 200) {
+        parseData(body);
+    }
+    else {
+        console.log("error while fetching url");
+    }
 });
 
-function parseData(html){
-    const {JSDOM} = jsdom;
-    const dom = new JSDOM(html);
-    const $ = (require('jquery'))(dom.window);
-    //let's start extracting the data
-    var items = $("#course_list");
-    // console.log(items[0].rows.length);
-    // console.log(items[0].rows[119].cells[1].children[0].getAttribute('href'));
-    // console.log(items[0].rows[119].cells[0].children[0].innerHTML);
-    // console.log(items[0].rows[119].cells[1].children[0].innerHTML);
-    // console.log(items[0].rows[119].cells[1].children[0].getAttribute('href'));
-    for(var i = 1; i < items[0].rows.length; i++){
-        // Print course number
-        console.log(items[0].rows[i].cells[0].children[0].innerHTML);
-
-        // Print course name
-        console.log(items[0].rows[i].cells[1].children[0].innerHTML);
-
-        // Print course link
-        console.log(items[0].rows[i].cells[1].children[0].getAttribute('href'));
+function parseData(html) {
+    const items = $("#course_list");
+    let list = [];
+    for (var i = 1; i < items[0].rows.length; i++) {
+        list.push({
+            courseNumber: items[0].rows[i].cells[0].children[0].innerHTML,
+            courseTitle: items[0].rows[i].cells[1].children[0].innerHTML,
+            link: items[0].rows[i].cells[1].children[0].getAttribute('href')
+        });
     }
+
+    fs.writeFile('courses.json', JSON.stringify(list), function (err) {
+        if (err) {
+            console.log(err)
+        } else { 
+            console.log('complete') 
+        }
+    });
 }
