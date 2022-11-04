c357e7855cab1c9e128c12d733f55983fef16d4c
yijingw37 <31359897+yijingw37@users.noreply.github.com>
2022-11-03 23:19:48 -0500

    added module

--- index.js
+++ index.js
@@ -1,60 +1,102 @@
-const curl = require("curl");
+import fetch from 'node-fetch';
+import "./addRequire.js";
 const jsdom = require("jsdom");
 const fs = require('fs');
-const url = "https://www.mccormick.northwestern.edu/computer-science/academics/courses/";
 let list = [];
+let course;
 
-// scrape course list
-curl.get(url, null, async (err, resp, body) => {
-    if (resp.statusCode == 200) {
-        parseData(body);
+// download page
+async function download(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Error! status: ${response.status}`);
+        }
+        const result = await response.text();
+        return result;
+    } catch (err) {
+        console.log(err);
     }
-    else {
-        //some error handling
-        console.log("error while fetching url");
-    }
-});
+}
 
-function parseData(html) {
-    const { JSDOM } = jsdom;
-    const dom = new JSDOM(html);
-    const $ = (require('jquery'))(dom.window);
-    const items = $("#course_list");
-    for (var i = 1; i < items[0].rows.length; i++) {
-        const courseUrl = items[0].rows[i].cells[1].children[0].getAttribute('href');
-        let course = {
-            courseNumber: items[0].rows[i].cells[0].children[0].innerHTML,
-            courseTitle: items[0].rows[i].cells[1].children[0].innerHTML,
-            link: courseUrl,
-            prerequisites: ""
-        };
-        list.push(course);
-
-        // Scrape each page for prerequisites
-        curl.get(url + courseUrl, null, async (err, resp, body) => {
-            if (resp.statusCode == 200) {
-                const { JSDOM } = jsdom;
-                const dom = new JSDOM(body);
-                const $ = (require('jquery'))(dom.window);
-                const prerequisites = $('h3:contains("Prerequisites")')[0];
-                if (prerequisites) {
-                    course.prerequisites = prerequisites.nextSibling.textContent;
-                }
-                list.push(course);
-            }
-            else {
-                //some error handling
-                console.log("error while fetching url");
-            }
-        });
-    }
-
-    // write to courses.json
-    fs.writeFile('../webhook/courses2.json', JSON.stringify(list), function (err) {
+// write to cache
+async function writeToCache(filePath, url, callback, i) {
+    fs.writeFile(filePath, await download(url), function (err) {
         if (err) {
             console.log(err)
         } else {
-            console.log('complete')
+            callback(filePath, i);
         }
     });
 }
+
+function generateCourseJson() {
+    // If course list doesn't exist in cache, download it
+    if (!fs.existsSync('./html/courses.html')) {
+        writeToCache('./html/courses.html', 'https://www.mccormick.northwestern.edu/computer-science/academics/courses/', readCourseList);
+    } else {
+        readCourseList('./html/courses.html');
+    }
+}
+
+function readCourseList(path) {
+    fs.readFile(path, { encoding: 'utf-8' }, function (err, data) {
+        if (!err) {
+            const { JSDOM } = jsdom;
+            const dom = new JSDOM(data);
+            const $ = (require('jquery'))(dom.window);
+            const items = $("#course_list");
+            // items[0].rows.length
+            for (var i = 1; i < 10; i++) {
+                const courseUrl = items[0].rows[i].cells[1].children[0].getAttribute('href');
+                course = {
+                    courseNumber: items[0].rows[i].cells[0].children[0].innerHTML,
+                    courseTitle: items[0].rows[i].cells[1].children[0].innerHTML,
+                    link: courseUrl,
+                    prerequisites: ''
+                };
+
+                generatePrerequisites(items[0].rows[i].cells[0].children[0].innerHTML, courseUrl);
+            }
+        } else {
+            console.log(err);
+        }
+    });
+}
+
+function generatePrerequisites(name, courseUrl) {
+    const path = './html/' + name + '.html';
+    if (!fs.existsSync(path)) {
+        writeToCache(path, 'https://www.mccormick.northwestern.edu/computer-science/academics/courses/' + courseUrl, readCourse);
+    } else {
+        readCourse(path);
+    }
+}
+
+function readCourse(path) {
+    fs.readFile(path, { encoding: 'utf-8' }, function (err, data) {
+        if (!err) {
+            const { JSDOM } = jsdom;
+            const dom = new JSDOM(data);
+            const $ = (require('jquery'))(dom.window);
+            const prerequisites = $('h3:contains("Prerequisites")')[0];
+            if (prerequisites) {
+                console.log("setting prereq", prerequisites.nextSibling.textContent)
+                course.prerequisites = prerequisites.nextSibling.textContent;
+            }
+            list.push(course);
+            fs.writeFile('../webhook/courses2.json', JSON.stringify(list), function (err) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log('complete writing to courses2.json')
+                }
+            });
+        } else {
+            console.log(err);
+        }
+    });
+
+}
+
+generateCourseJson();
